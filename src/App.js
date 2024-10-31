d5c084293d3116bc2e5ba3a981d11a0c431dfd6e
MohamedAliMefteh <mohamedalimefteh.fl@gmail.com>
2024-10-31 16:37:52 +0100

    fc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from "axios"
+import UserList from './UserList';
 
 function App() {
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+  .then((info)=>{
+    setUsers(info.data)
+  })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {users.map((user)=>(
+        <UserList  user={user}/>
+        
+       ))}
+      
       </header>
     </div>
   );
